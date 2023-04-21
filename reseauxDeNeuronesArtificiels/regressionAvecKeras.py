f79a207d4c81775dafddf83e607d5e6495fba9c3
mathieusantiago <mathieusantiago21@gmail.com>
2023-04-21 20:10:57 +0200

    continue conrs regression avec keras

--- reseauxDeNeuronesArtificiels/regressionAvecKeras.py
+++ reseauxDeNeuronesArtificiels/regressionAvecKeras.py
@@ -1,31 +1,62 @@
-stateDev = True
-
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+stateDev = True
 
 path = 'DATA/kc_house_data.csv'
 abs_path = os.path.abspath(path)
 df = pd.read_csv(abs_path)
+
 
 def devState():
     if stateDev:
         return plt.show()
     else:
         return print('  devState not display graph ')
-    
+
 
 print(df.isnull().sum())
 
 df.describe().transpose
 
-plt.figure(figsize=(10,6))
-sns.distplot(df['price'])
-devState()
+plt.figure(figsize=(10, 6))
+sns.histplot(df['price'])
+# devState()
 
-sns.countplot(df['bedrooms'])
-devState()
+sns.countplot(x=df['bedrooms'])
+# devState()
 
-df.corr()
+plt.figure(figsize=(12, 8))
+sns.scatterplot(x='price', y='sqft_living', data=df)
+# devState()
+
+plt.figure(figsize=(12, 8))
+sns.boxplot(x='bedrooms', y='price', data=df)
+# devState()
+
+plt.figure(figsize=(12, 8))
+sns.scatterplot(x='price', y='long', data=df)
+# devState()
+
+plt.figure(figsize=(12, 8))
+sns.scatterplot(x='price', y='lat', data=df)
+# devState()
+
+plt.figure(figsize=(12, 8))
+sns.scatterplot(x='long', y='lat', data=df, hue='price')
+# devState()
+
+print(df.sort_values('price', ascending=False).head(20))
+
+non_top_1_perc = df.sort_values('price', ascending=False).iloc[216:]
+
+plt.figure(figsize=(12, 8))
+sns.scatterplot(x='long', y='lat', data=non_top_1_perc,
+                hue='price', edgecolor=None, alpha=0.2, palette='RdYlGn')
+# devState()
+
+plt.figure(figsize=(12, 8))
+sns.boxenplot(x='waterfront', y='price', data=df)
+devState()
