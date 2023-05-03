280525271d066e8448e6e679d8f85d2edb574c5e
mathieusantiago <mathieusantiago21@gmail.com>
2023-05-03 23:14:09 +0200

    add dataset

--- reseauxDeNeuronesArtificiels/classificationAvecKeras.py
+++ reseauxDeNeuronesArtificiels/classificationAvecKeras.py
@@ -4,3 +4,7 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
+# Importer le jeu de données
+path = 'DATA/fake_reg.csv'
+abs_path = os.path.abspath(path)
+df = pd.read_csv(abs_path)
